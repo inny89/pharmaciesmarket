b141de8f7f0010d06ccb0a732ba50ed26bfaa3a5
milosz <milosztadrzak@gmail.com>
2018-06-30 10:32:16 +0200

    Configured PostgreSql and WildFly as

---
+++ src/main/java/com/mita/pharmaciesmarket/PharmaciesmarketApplication.java
@@ -0,0 +1,12 @@
+package com.mita.pharmaciesmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaciesmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaciesmarketApplication.class, args);
+	}
+}
---
+++ src/main/java/com/mita/pharmaciesmarket/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.mita.pharmaciesmarket;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PharmaciesmarketApplication.class);
+	}
+
+}
